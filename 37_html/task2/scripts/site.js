8ee8a348fe9b59d02b527202e0512db849781ad3
TheHACKATHON <okorokmahjonga@gmail.com>
2019-10-25 16:45:38 +0300

    37_html>task2 js revorked

--- 37_html/task2/scripts/site.js
+++ 37_html/task2/scripts/site.js
@@ -4,57 +4,51 @@ const newTaskInput = document.querySelector("input#taskName");
 const newTaskSubmit = document.querySelector("button#taskSubmit");
 const ulTodo = document.querySelector("ul.todo");
 
-// клик по li переключает класс checked
-// li: HTMLElement
-const addLiEventClick = function(li) {
-    li.addEventListener("click", function () {
-        this.classList.toggle("checked");
-    });
-};
-
-// добавление к li кнопки удаления
-// li: HTMLElement
-const addLiRemoveBtn = function(li) {
-    const btn = document.createElement("a");
-    btn.classList.add("remove-btn");
-    addRemoveBtnEventClick(btn);
-    li.appendChild(btn);
-};
-
-// добавления нового задания в лист
-// task : string - название
-const addTask = function(task){
-    const li = document.createElement("li");
-    li.textContent = task;
-    addLiEventClick(li);
-    addLiRemoveBtn(li);
-    if(ulTodo.firstChild != null){
-        ulTodo.insertBefore(li, ulTodo.firstChild);
-    } else{
-        ulTodo.appendChild(li);
-    }
-};
-
-// добавление события клик на кнопку удаления
-// btn: HTMLElement
-const addRemoveBtnEventClick = function(btn){
-    btn.addEventListener("click", function(){
-        this.parentNode.remove();        
-    });
+const liFunctions = {
+    // клик по li переключает класс checked
+    // li: HTMLElement
+    addEventClick: function (li) {
+        li.addEventListener("click", function () {
+            this.classList.toggle("checked");
+        });
+    },
+    // добавление к li кнопки удаления
+    // li: HTMLElement
+    appendRemoveBtn: function (li) {
+        const btn = document.createElement("a");
+        btn.classList.add("remove-btn");
+        btn.addEventListener("click", function () {
+            this.parentNode.remove();
+        });
+        li.appendChild(btn);
+    },
+    // добавления нового задания в лист
+    // task: string - название
+    createAndAppendTask: function (task) {
+        const li = document.createElement("li");
+        li.textContent = task;
+        liFunctions.addEventClick(li);
+        liFunctions.appendRemoveBtn(li);
+        if (ulTodo.firstChild != null) {
+            ulTodo.insertBefore(li, ulTodo.firstChild);
+        } else {
+            ulTodo.appendChild(li);
+        }
+    },
 };
 
 // добавление событий на существующие todo>li при загрузке страницы
 todoList.forEach(li => {
-    addLiEventClick(li);
-    addLiRemoveBtn(li);
+    liFunctions.addEventClick(li);
+    liFunctions.appendRemoveBtn(li);
 });
 
 newTaskSubmit.addEventListener("click", () => {
     const task = newTaskInput.value.trim();
     newTaskInput.value = "";
-    if(task.length == 0){
+    if (task.length == 0) {
         alert("enter a task name");
         return;
     }
-    addTask(task);
+    liFunctions.createAndAppendTask(task);
 });
