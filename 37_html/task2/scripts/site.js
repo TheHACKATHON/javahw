3240a56af707517c77353ddb6678e1207884950e
TheHACKATHON <okorokmahjonga@gmail.com>
2019-10-25 16:17:52 +0300

    37_html>task2 updated

--- 37_html/task2/scripts/site.js
+++ 37_html/task2/scripts/site.js
@@ -4,16 +4,30 @@ const newTaskInput = document.querySelector("input#taskName");
 const newTaskSubmit = document.querySelector("button#taskSubmit");
 const ulTodo = document.querySelector("ul.todo");
 
+// клик по li переключает класс checked
+// li: HTMLElement
 const addLiEventClick = function(li) {
     li.addEventListener("click", function () {
         this.classList.toggle("checked");
     });
 };
 
+// добавление к li кнопки удаления
+// li: HTMLElement
+const addLiRemoveBtn = function(li) {
+    const btn = document.createElement("a");
+    btn.classList.add("remove-btn");
+    addRemoveBtnEventClick(btn);
+    li.appendChild(btn);
+};
+
+// добавления нового задания в лист
+// task : string - название
 const addTask = function(task){
     const li = document.createElement("li");
     li.textContent = task;
     addLiEventClick(li);
+    addLiRemoveBtn(li);
     if(ulTodo.firstChild != null){
         ulTodo.insertBefore(li, ulTodo.firstChild);
     } else{
@@ -21,8 +35,18 @@ const addTask = function(task){
     }
 };
 
+// добавление события клик на кнопку удаления
+// btn: HTMLElement
+const addRemoveBtnEventClick = function(btn){
+    btn.addEventListener("click", function(){
+        this.parentNode.remove();        
+    });
+};
+
+// добавление событий на существующие todo>li при загрузке страницы
 todoList.forEach(li => {
     addLiEventClick(li);
+    addLiRemoveBtn(li);
 });
 
 newTaskSubmit.addEventListener("click", () => {
